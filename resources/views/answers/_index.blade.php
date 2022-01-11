be837159799681e9d7434dca11073c093970b091
Sudip Singh <sudipsingh04@gmail.com>
2022-01-11 18:48:15 +0530

    Accept amswer as best answer

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -17,9 +17,26 @@
                             <a title="This answer is not useful" class="vote-down off">
                                 <i class="fas fa-caret-down fa-3x"></i>
                             </a>
-                            <a title="Click to mark as favorite answer (Click again to undo)" class="{{ $answer->status }} mt-2">
-                                <i class="fas fa-check fa-2x"></i>
-                            </a>
+                            @can('accept', $answer)
+                                <a title="Click to mark as favorite answer (Click again to undo)"
+                                class="{{ $answer->status }} mt-2"
+                                onclick="event.preventDefault(); document.getElementById('accept-answer-{{$answer->id}}').submit();"
+                                >
+                                    <i class="fas fa-check fa-2x"></i>
+                                </a>
+                                <form id="accept-answer-{{$answer->id}}" action="{{ route('answers.accept', $answer->id) }}" method="POST" style="display: none">
+                                    @csrf
+                                </form>
+                            @else
+                                @if ($answer->is_best)
+                                    <a title="The question owner accepted this answer as favorite answer (Click again to undo)"
+                                    class="{{ $answer->status }} mt-2"
+                                    onclick="event.preventDefault(); document.getElementById('accept-answer-{{$answer->id}}').submit();"
+                                    >
+                                        <i class="fas fa-check fa-2x"></i>
+                                    </a>
+                                @endif
+                            @endcan
                         </div>
                         <div class="media-body">
                             {!! $answer->body_html !!}
@@ -27,9 +44,9 @@
                                 <div class="col-4">
                                     <div class="ml-auto">
                                         @can('update', $answer)
-                                            <a href="{{route('questions.answers.edit', [$question->id, $answer->id])}}" class="btn btn-sm btn-outline-info">Edit</a>  
+                                            <a href="{{route('questions.answers.edit', [$question->id, $answer->id])}}" class="btn btn-sm btn-outline-info">Edit</a>
                                         @endcan
-                                        
+
                                         @can('delete', $answer)
                                             <form class="form-delete" action="{{route('questions.answers.destroy', [$question->id, $answer->id])}}" method="post">
                                                 @method('delete')
